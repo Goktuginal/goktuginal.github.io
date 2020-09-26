a0bffd593f1b9cfeb117e8602587b13dac84aa84
ELMA <GOKTUG@ELMA-MacBook-Pro.local>
2020-09-26 17:38:31 +0200

    Initial commit

--- threejs/color-cube.js
+++ threejs/color-cube.js
@@ -1,72 +1,70 @@
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r119/build/three.module.js';
 
-// Variables de consenso
-// Motor, escena y la camara
-var renderer, scene, camera;
+function main() {
+  const canvas = document.querySelector('#c');
+  const renderer = new THREE.WebGLRenderer({canvas});
 
-// Otras globales
-var esferaCubo, angulo = 0;
+  const fov = 75;
+  const aspect = 2;  // the canvas default
+  const near = 0.1;
+  const far = 100;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 5;
 
-// Acciones
-init();
-loadScene();
-render();
+  const scene = new THREE.Scene();
 
-function init() {
+  {
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+  }
 
-	// Configurar el motor de render y el canvas
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.setClearColor(new THREE.Color(0x0000AA));
-	document.getElementById("container").appendChild(renderer.domElement);
+  const geometry = new THREE.Geometry();
+  geometry.vertices.push(
+    new THREE.Vector3(-1, -1,  1),  // 0
+    new THREE.Vector3( 1, -1,  1),  // 1
+    new THREE.Vector3(-1,  1,  1),  // 2
+    new THREE.Vector3( 1,  1,  1),  // 3
+    new THREE.Vector3(-1, -1, -1),  // 4
+    new THREE.Vector3( 1, -1, -1),  // 5
+    new THREE.Vector3(-1,  1, -1),  // 6
+    new THREE.Vector3( 1,  1, -1),  // 7
+  );
 
-	// Escena
-	scene = new THREE.Scene();
+  /*
+       6----7
+      /|   /|
+     2----3 |
+     | |  | |
+     | 4--|-5
+     |/   |/
+     0----1
+  */
 
-	// Camara
-	var ar = window.innerWidth / window.innerHeight;
-	camera = new THREE.PerspectiveCamera(50, ar, 0.1, 100);
-	scene.add(camera);
-	camera.position.set(0.5, 3, 9);
-	camera.lookAt(new THREE.Vector3(0, 2, 0));
-}
+  geometry.faces.push(
+     // front
+     new THREE.Face3(0, 3, 2),
+     new THREE.Face3(0, 1, 3),
+     // right
+     new THREE.Face3(1, 7, 3),
+     new THREE.Face3(1, 5, 7),
+     // back
+     new THREE.Face3(5, 6, 7),
+     new THREE.Face3(5, 4, 6),
+     // left
+     new THREE.Face3(4, 2, 6),
+     new THREE.Face3(4, 0, 2),
+     // top
+     new THREE.Face3(2, 7, 6),
+     new THREE.Face3(2, 3, 7),
+     // bottom
+     new THREE.Face3(4, 1, 0),
+     new THREE.Face3(4, 5, 1),
+  );
 
-function loadScene() {
-	
-	var malla = new THREE.Geometry();
-	malla.vertices.push(
-		new THREE.Vector3(-1, -1,  1), //0
-		new THREE.Vector3( 1, -1,  1), //1
-		new THREE.Vector3(-1,  1,  1), //2
-		new THREE.Vector3( 1,  1,  1), //3
-		new THREE.Vector3(-1, -1, -1), //4
-		new THREE.Vector3( 1, -1, -1), //5
-		new THREE.Vector3(-1,  1, -1), //6
-		new THREE.Vector3(-1,  1, -1), //7
-		);
-
-	malla.faces.push(
-		// front
-		new THREE.Face3(0, 3, 2),
-		new THREE.Face3(0, 1, 3),
-		// right
-		new THREE.Face3(1, 7, 3),
-		new THREE.Face3(1, 5, 7),
-		// back
-		new THREE.Face3(5, 6, 7),
-		new THREE.Face3(5, 4, 6),
-		// left
-		new THREE.Face3(4, 2, 6),
-		new THREE.Face3(4, 0, 2),
-		// top
-		new THREE.Face3(2, 7, 6),
-		new THREE.Face3(2, 3, 7),
-		// bottom
-		new THREE.Face3(4, 1, 0),
-		new THREE.Face3(4, 5, 1),
-		);
-}
-
-function makeInstance(geometry, color, x) {
+  function makeInstance(geometry, color, x) {
     const material = new THREE.MeshBasicMaterial({color});
 
     const cube = new THREE.Mesh(geometry, material);
@@ -76,26 +74,45 @@ function makeInstance(geometry, color, x) {
     return cube;
   }
 
- var cubes = makeInstance(geometry, 0x44FF44,  0);
+  const cubes = [
+    makeInstance(geometry, 0x44FF44,  0),
+    makeInstance(geometry, 0x4444FF, -4),
+    makeInstance(geometry, 0xFF4444,  4),
+  ];
 
-function update() {
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
 
-	// Variacion de la escena entre frames
-	/*angulo += Math.PI/100;
-	esferacubo.rotation.y = angulo;*/
+  function render(time) {
+    time *= 0.001;
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * .1;
+      const rot = time * speed;
+      cube.rotation.x = rot;
+      cube.rotation.y = rot;
+    });
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
 }
 
-function render() {
-	
-	// Construir el frame y mostrarlo
-	requestAnimationFrame(render);
-	update();
-	renderer.render(scene, camera);
-}
-
-
-
-
-
-
-
+main();
