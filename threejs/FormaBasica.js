0353bd3ab8e2416f814aba5f21d95bd6c20c8270
ELMA <GOKTUG@ELMA-MacBook-Pro.local>
2020-10-16 18:36:07 +0200

    Initial commit

--- threejs/FormaBasica.js
+++ threejs/FormaBasica.js
@@ -53,7 +53,7 @@ function loadScene() {
 	var ba = new THREE.Mesh(cylinder, material);
 
 	var ground = new THREE.Mesh(cylinder2, material);
-	ground.rotation.z = 90;
+	ground.rotation.x = 180;
 	var head = new THREE.Mesh(sphere, material);
 	head.position.y = 3;
 	head.rotation.y = 45;
