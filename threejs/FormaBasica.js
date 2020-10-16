895d7f31e793b834e4d36f46fb4d12990fa129a2
ELMA <GOKTUG@ELMA-MacBook-Pro.local>
2020-10-16 17:48:46 +0200

    Initial commit

--- threejs/FormaBasica.js
+++ threejs/FormaBasica.js
@@ -47,6 +47,7 @@ function loadScene() {
 	var cylinder = new THREE.CylinderGeometry(1.25, 1.25, 0.15, 30);
 	var cylinder2 = new THREE.CylinderGeometry(0.55, 0.55, 0.18, 30);
 	var sphere = new THREE.SphereGeometry(0.55, 30, 30);
+	var line = new THREE.Line3(0, 0, 0, 5, 5, 5)
 	//var geoesfera = new THREE.SphereGeometry(1, 3, 3);
 
 	// Objetos
@@ -58,7 +59,8 @@ function loadScene() {
 	var head = new THREE.Mesh(sphere, material);
 	head.position.y = 3;
 	ground.rotation.z = 45;
-	
+
+	var body = new THREE.Mesh(line, material);
 	// Orden de las transformaciones TRS
 	//cubo.rotation.y = Math.PI/4;
 	//cubo.position.x = -1;
@@ -81,6 +83,7 @@ function loadScene() {
 	});*/
 
 	//Organizacion de la escena
+	brazo.add(line);
 	brazo.add(head);
 	brazo.add(ground);
 	head.add( new THREE.AxisHelper(4));
