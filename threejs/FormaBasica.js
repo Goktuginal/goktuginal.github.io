db21c7517f048864825c0d8bb4b570facf3ec786
ELMA <GOKTUG@ELMA-MacBook-Pro.local>
2020-10-16 15:51:28 +0200

    Initial commit

--- threejs/FormaBasica.js
+++ threejs/FormaBasica.js
@@ -44,7 +44,7 @@ function loadScene() {
 	var material = new THREE.MeshBasicMaterial({color: 'yellow', wireframe: true});
 
 	// Geometrias
-	var cylinder = new THREE.CylinderGeometry(5, 5, 0.15);
+	var cylinder = new THREE.CylinderGeometry(2.5, 2.5, 1.5);
 	//var geoesfera = new THREE.SphereGeometry(1, 3, 3);
 
 	// Objetos
