b22ff7a567f4d9dc225eda0a19444243bd4d1d37
ELMA <GOKTUG@ELMA-MacBook-Pro.local>
2020-10-18 14:23:51 +0200

    Initial commit

--- threejs/FormaBasica.js
+++ threejs/FormaBasica.js
@@ -141,13 +141,13 @@ function loadScene() {
 	cubo = new THREE.Mesh(malla, material);
 	cubo.position.x = -0.3;
 	cubo.position.y = 5;
-	cubo.position.z = -1;
-	cubo.rotation.y = 25;
+	cubo.position.z = 0;
+	cubo.rotation.y = 35;
 	cubo2 = new THREE.Mesh(malla, material);
 	cubo2.position.x = 0.3;
 	cubo2.position.y = 5;
-	cubo2.position.z = -1;
-	cubo2.rotation.y = 25;
+	cubo2.position.z = 0;
+	cubo2.rotation.y = 35;
 
 
 	// Objeto contenedor
