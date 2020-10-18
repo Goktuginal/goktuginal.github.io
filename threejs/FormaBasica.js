53a6ccd060f59e61e907fe1e495bec80cd02bdfd
ELMA <GOKTUG@ELMA-MacBook-Pro.local>
2020-10-18 15:44:31 +0200

    Initial commit

--- threejs/FormaBasica.js
+++ threejs/FormaBasica.js
@@ -140,11 +140,10 @@ function loadScene() {
 		malla.faces.push(triangulo);
 	}
 	cubo = new THREE.Mesh(malla, material2);
-	cubo.position.x = -0.3;
 	cubo.position.y = 5;
 	cubo.rotation.y = 45;
 	cubo2 = new THREE.Mesh(malla, material2);
-	cubo2.position.x = 1.3;
+	cubo2.position.x = 0.3;
 	cubo2.position.y = 5;
 	cubo2.rotation.y = 45;
 
