7ceb9998e781e22f51d2b7c51145fbeb232bd098
ELMA <GOKTUG@ELMA-MacBook-Pro.local>
2020-10-18 14:09:32 +0200

    Initial commit

--- threejs/FormaBasica.js
+++ threejs/FormaBasica.js
@@ -111,14 +111,14 @@ function loadScene() {
 					-semilado/4,  semilado/2, -semilado,
 					-semilado/4, -semilado/2, -semilado,
 					-semilado/4, -semilado/2,  semilado,
-					 semilado/8, -semilado/4,  semilado*4,
+					 semilado/8, -semilado/4,  semilado*2,
 					 semilado/4, -semilado/2,  semilado,
 					 semilado/4,  semilado/2,  semilado,
-					 semilado/8,  semilado/4,  semilado*4,
-					-semilado/8,  semilado/4,  semilado*4,
+					 semilado/8,  semilado/4,  semilado*2,
+					-semilado/8,  semilado/4,  semilado*2,
 					-semilado/4,  semilado/2,  semilado,
 					-semilado/4, -semilado/2,  semilado,
-					-semilado/8, -semilado/4,  semilado*4	];
+					-semilado/8, -semilado/4,  semilado*2	];
 
 	var indices = [
 				0,3,7, 7,3,4, 0,1,2,	
