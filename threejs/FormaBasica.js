c1245a0d2f27d9ddda23474929e334eb254545c5
ELMA <GOKTUG@ELMA-MacBook-Pro.local>
2020-10-20 14:08:31 +0200

    Initial commit

--- threejs/FormaBasica.js
+++ threejs/FormaBasica.js
@@ -11,11 +11,38 @@ var renderer, scene, camera;
 
 // Otras globales
 var esferaCubo, angulo = 0;
+var l = b = -4;
+var r = t = -l;
+var cameraControls;
 
 // Acciones
 init();
 loadScene();
 render();
+
+function setCameras(ar) {
+
+	// Construye las camaras planta, alzado, perfil y perspectiva
+	var origen = new THREE.Vector3(0, 0, 0);
+
+	if (ar > 1) {
+		var camaraOrtografica = new THREE.OrthographicCamera(l*ar, r*ar, t, b, -20, 20);
+	}
+	else {
+		var camaraOrtografica = new THREE.OrthographicCamera(l, r, t/ar, b/ar, -20, 20);
+	}
+
+	// Camara perspectiva
+	var camaraPerspectiva = new THREE.PerspectiveCamera(50, ar, 0.1, 50);
+	camaraPerspectiva.position.set(1, 2, 10);
+	camaraPerspectiva.lookAt(origen);
+
+	camera = camaraPerspectiva.clone();
+
+	
+	scene.add(camera);
+
+}
 
 function init() {
 
@@ -30,10 +57,92 @@ function init() {
 
 	// Camara
 	var ar = window.innerWidth / window.innerHeight;
-	camera = new THREE.PerspectiveCamera(50, ar, 0.1, 100);
+	/*camera = new THREE.PerspectiveCamera(50, ar, 0.1, 100);
 	scene.add(camera);
 	camera.position.set(0.5, 3, 9);
-	camera.lookAt(new THREE.Vector3(0, 2, 0));
+	camera.lookAt(new THREE.Vector3(0, 2, 0));*/
+
+	setCameras(ar);
+
+	// Control de camara
+	cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
+	cameraControls.target.set(0, 0, 0);
+	cameraControls.noKeys = true;
+
+	// Captura de eventos
+	window.addEventListener('resize', updateAspectRation);
+	renderer.domElement.addEventListener('dblclick', rotate);
+}
+
+function rotate(event) {
+
+	// Gira el objeto senyalado 45 grados
+	var x = event.clientX;
+	var y = event.clientY;
+
+	var derecha = false, abajo = false;
+	var cam = null;
+
+	// Cuadrante para la x,y?
+	if (x > window.innerWidth/2) {
+		x -= window.innerWidth/2;
+		derecha = true;
+	};
+	if (y > window.innerHeight/2) {
+		y -= window.innerHeight/2;
+		abajo = true;
+	};
+	if (derecha)
+		if (abajo)	cam = camera;
+		else 	cam = perfil;
+	else 
+		if (abajo) cam = planta;
+		else cam = alzado;
+		
+
+	// Transformacion a cuadrado de 2x2
+	x = (2 * x / window.innerWidth) * 2 - 1;
+	y = -(2 * y / window.innerHeight) * 2 + 1;
+
+	console.log(x + "," + y);
+	var rayo = new THREE.Raycaster();
+	rayo.setFromCamera(new THREE.vector2(x, y), camera);
+
+	var interseccion = rayo.intersectObjects(scene.children, true);
+
+	if (interseccion.length > 0) {
+		interseccion[0].object.rotation.y += Math.PI/4;
+	}
+}
+
+function updateAspectRation(argument) {
+
+	// Renueva la relacion de aspecto de la camara
+
+	// Ajustar el tamano del canvas
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	// Razon de aspecto
+	var ar = window.innerWidth/window.innerHeight;
+
+	/* Camara ortografica
+	if (ar > 1) { //büyük ise ekranın sol ve sağına tersinde aşağı ve yukarı
+		camera.left = -4*ar;
+		camera.right = 4*ar;
+		camera.bottom = -4;
+		camera.top = 4;
+	}
+	else {
+		camera.top = 4/ar;
+		camera.bottom = -4/ar;
+		camera.left = -4;
+		camera.right = 4;
+	}
+*/
+
+	// Camara perspectiva
+	camera.aspect = ar;
+	camera.updateProjectionMatrix();
 }
 
 function loadScene() {
@@ -194,6 +303,15 @@ function render() {
 	// Construir el frame y mostrarlo
 	requestAnimationFrame(render);
 	update();
+
+	// Thumnail
+	renderer.setViewport(0, 0, 
+						window.innerWidth/4, window.innerHeight/4);
+	renderer.render(scene, camera); 	
+
+	// Robot
+	renderer.setViewport(window.innerWidth/4, window.innerHeight/4, 
+						window.innerWidth/2, window.innerHeight/2);
 	renderer.render(scene, camera);
 }
 
