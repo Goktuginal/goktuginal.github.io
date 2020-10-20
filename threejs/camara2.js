bf52742ff92acddee88021622cdfcc399497acf2
ELMA <GOKTUG@ELMA-MacBook-Pro.local>
2020-10-20 13:14:49 +0200

    Initial commit

--- threejs/camara2.js
+++ threejs/camara2.js
@@ -224,13 +224,13 @@ function render() {
 						window.innerWidth/2, window.innerHeight/2);
 
 	// Aynı anda çalıştırabilirsin.
-	renderer.render(scene, perfil); 	// Yandan göstermesi gerekiyor
+	renderer.render(scene, planta); 	// Yandan göstermesi gerekiyor
 	renderer.setViewport(0, window.innerHeight/2, 
 						window.innerWidth/2, window.innerHeight/2);
 	renderer.render(scene, alzado); 	// ? göstermesi gerekiyor
 	renderer.setViewport(0, 0, 
 						window.innerWidth/2, window.innerHeight/2);
-	renderer.render(scene, planta); 	// Tepeden göstermesi gerekiyor
+	renderer.render(scene, perfil); 	// Tepeden göstermesi gerekiyor
 	renderer.setViewport(window.innerWidth/2, 0, 
 						window.innerWidth/2, window.innerHeight/2);
 	renderer.render(scene, camera);
