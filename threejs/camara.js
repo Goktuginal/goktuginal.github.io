5d433e077b5a7a8bcebdfb59258725be9f634984
ELMA <GOKTUG@ELMA-MacBook-Pro.local>
2020-10-22 13:49:13 +0200

    Initial commit

--- threejs/camara.js
+++ threejs/camara.js
@@ -5,10 +5,16 @@
 *
 */
 
+"use strict";
 
 // Variables de consenso
 // Motor, escena y la camara
 var renderer, scene, camera;
+
+// Monitor de recursos
+var stats;
+// Global GUI
+var effectController;
 
 // Otras globales
 var esferacubo, cubo, angulo = 0;
@@ -17,9 +23,15 @@ var r = t = -l;
 var cameraControls;
 var alzado, planta, perfil;
 
+// Objetos y tiempo
+var peonza,eje;
+var antes = Date.now();
+
 // Acciones
 init();
 loadScene();
+setupGui();
+startAnimation();
 render();
 
 function init() {
@@ -49,41 +61,18 @@ function init() {
 	cameraControls.target.set(0, 0, 0);
 	cameraControls.noKeys = true;
 
+	// STATS --> stats.update() en update()
+	stats = new Stats();
+	stats.setMode( 0 );					// Muestra FPS
+	stats.domElement.style.position = 'absolute';		// Abajo izquierda
+	stats.domElement.style.bottom = '0px';
+	stats.domElement.style.left = '0px';
+	document.getElementById( 'container' ).appendChild( stats.domElement );
+
 	// Captura de eventos
 	window.addEventListener('resize', updateAspectRation);
 
 }
-
-function updateAspectRation(argument) {
-
-	// Renueva la relacion de aspecto de la camara
-
-	// Ajustar el tamano del canvas
-	renderer.setSize(window.innerWidth, window.innerHeight);
-
-	// Razon de aspecto
-	var ar = window.innerWidth/window.innerHeight;
-
-	/* Camara ortografica
-	if (ar > 1) { //büyük ise ekranın sol ve sağına tersinde aşağı ve yukarı
-		camera.left = -4*ar;
-		camera.right = 4*ar;
-		camera.bottom = -4;
-		camera.top = 4;
-	}
-	else {
-		camera.top = 4/ar;
-		camera.bottom = -4/ar;
-		camera.left = -4;
-		camera.right = 4;
-	}
-*/
-
-	// Camara perspectiva
-	camera.aspect = ar;
-	camera.updateProjectionMatrix();
-}
-
 
 function loadScene() {
 
@@ -118,6 +107,10 @@ function loadScene() {
 	esferacubo.add(esfera);
 	scene.add(esferacubo);
 	scene.add( new THREE.AxisHelper(3));
+
+	//Coordinates.drawGrid({size:6,scale:1});
+	Coordinates.drawGrid({size:6,scale:1, orientation:"y"});
+	Coordinates.drawGrid({size:6,scale:1, orientation:"z"});	
 
 	var keyboard = new THREEx.KeyboardState(renderer.domElement);
 	renderer.domElement.setAttribute("tabIndex", "0");
@@ -165,11 +158,115 @@ function loadScene() {
 		})
 	})
 }
+
+
+function setupGui()
+{
+	// Definicion de los controles
+	effectController = {
+		mensaje: 'Interfaz',
+		velang: 1,
+		reiniciar: function(){
+			TWEEN.removeAll();
+			cubo.position.set(-2.5,0,-2.5);
+			cubo.rotation.set( 0, 0, 0 );
+			startAnimation();
+		},
+		sombras: true,
+		color: "rgb(255,0,0)"
+	};
+
+	// Creacion interfaz
+	var gui = new dat.GUI();
+
+	// Construccion del menu
+	var h = gui.addFolder("Control peonza");
+	h.add(effectController, "mensaje").name("Peonza");
+	h.add(effectController, "velang", 0, 5, 0.5).name("Vueltas/sg");
+	h.add(effectController, "reiniciar").name("Reiniciar");
+	var sensorColor = h.addColor(effectController, "color").name("Color");
+	sensorColor.onChange( function(color){
+							esferacubo.traverse( function(hijo){
+								if( hijo instanceof THREE.Mesh ) hijo.material.color = new THREE.Color(color);
+							})
+						  });
+}
+
+function startAnimation(){
+	// Movimiento autonomo de la peonza mediante TWEEN
+	var mvtoDer = new TWEEN.Tween( cubo.position ).to( {x: [-1.5, -2.5],
+													 y: [0, 0],
+													 z: [0, 2.5] }, 5000 );
+	var mvtoFrente = new TWEEN.Tween( cubo.position ).to( {x: [0, 2.5],
+													 y: [0, 0],
+													 z: [0, 2.5] }, 5000 );
+	var mvtoIzq = new TWEEN.Tween( cubo.position ).to( {x: [1.5, 2.5],
+													 y: [0, 0],
+													 z: [0, -2.5] }, 5000 );
+	var mvtoTras = new TWEEN.Tween( cubo.position ).to( {x: [0, -2.5],
+													 y: [0, 0],
+													 z: [-1.5, -2.5] }, 5000 );
+
+	mvtoDer.easing(TWEEN.Easing.Bounce.Out);
+	mvtoDer.interpolation( TWEEN.Interpolation.Bezier );
+	mvtoFrente.easing(TWEEN.Easing.Bounce.Out);
+	mvtoFrente.interpolation( TWEEN.Interpolation.Bezier );
+	mvtoIzq.easing(TWEEN.Easing.Bounce.Out);
+	mvtoIzq.interpolation( TWEEN.Interpolation.Bezier );
+	mvtoTras.easing(TWEEN.Easing.Bounce.Out);
+	mvtoTras.interpolation( TWEEN.Interpolation.Bezier );
+
+	mvtoDer.chain( mvtoFrente );
+	mvtoFrente.chain( mvtoIzq );
+	mvtoIzq.chain( mvtoTras );
+	//mvto.repeat( 1 );
+	//mvto.yoyo( true );
+	mvtoDer.start();
+
+	// Giro de la peonza
+	var giro = new TWEEN.Tween( cubo.rotation ).to( {x:0, y:-Math.PI*2, z:0}, 2000 );
+	giro.repeat(Infinity);
+	giro.start();
+}
+
+function updateAspectRation(argument) {
+
+	// Renueva la relacion de aspecto de la camara
+
+	// Ajustar el tamano del canvas
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth/window.innerHeight;
+
+	/* Camara ortografica
+	if (ar > 1) { //büyük ise ekranın sol ve sağına tersinde aşağı ve yukarı
+		camera.left = -4*ar;
+		camera.right = 4*ar;
+		camera.bottom = -4;
+		camera.top = 4;
+	}
+	else {
+		camera.top = 4/ar;
+		camera.bottom = -4/ar;
+		camera.left = -4;
+		camera.right = 4;
+	}
+*/
+
+	// Camara perspectiva
+	camera.updateProjectionMatrix();
+}
+
 function update() {
 
 	// Variacion de la escena entre frames
 	// https://goktuginal.github.io/usocanvas.html
 
+	// Control de camra
+	cameraControls.update();
+	// Actualiza los FPS
+	stats.update();
+	// Actualiza interpoladores
+	TWEEN.update();
 }
 
 function render() {
