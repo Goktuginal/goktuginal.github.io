5ab51d80ec9423d0c16bbb1c3b95ceb6b90d73e0
ELMA <GOKTUG@ELMA-MacBook-Pro.local>
2020-10-22 18:44:56 +0200

    Initial commit

--- threejs/FormaBasica.js
+++ threejs/FormaBasica.js
@@ -161,7 +161,7 @@ function loadScene() {
 	brazo = new THREE.Object3D();
 	var wheel = new THREE.Mesh(new THREE.CylinderGeometry(0.5, 0.5, 0.3, 10, 2), material);
 	wheel.position.set(0, 0.25, 0);
-	wheel.rotation.z = Math.PI/4;
+	wheel.rotation.z = Math.PI/2;
 	brazo.add(wheel);
 	var legs = new THREE.Mesh(new THREE.BoxGeometry(0.3, 2.5, 0.3), material);
 	legs.position.set(0, 1.5, 0);
