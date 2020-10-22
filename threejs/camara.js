350142dfa8cc568b205f11d609cd208d4c9eaa1c
ELMA <GOKTUG@ELMA-MacBook-Pro.local>
2020-10-22 13:12:06 +0200

    Initial commit

--- threejs/camara.js
+++ threejs/camara.js
@@ -117,10 +117,6 @@ function loadScene() {
 	esferacubo.add(esfera);
 	scene.add(esferacubo);
 	scene.add( new THREE.AxisHelper(3));
-	/**
-	 * declare THREEx namespace
-	 */
-	var THREEx    = THREEx    || {};
 
 	var keyboard = new THREEx.KeyboardState(renderer.domElement);
 	renderer.domElement.setAttribute("tabIndex", "0");
