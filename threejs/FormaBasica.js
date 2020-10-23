6f75df90610013f441888fcad1ad98659386d02b
ELMA <GOKTUG@ELMA-MacBook-Pro.local>
2020-10-23 18:22:49 +0200

    Initial commit

--- threejs/FormaBasica.js
+++ threejs/FormaBasica.js
@@ -387,7 +387,7 @@ function update() {
 
 										// Actualizar antes
 	
-	antrebrazo.rotation.y = effectController.velang3*Math.PI/100;
+	antrebrazo.rotation.y = effectController.velang3*Math.PI/180;
 
 	var ahora4 = Date.now();							// Hora actual
 	angulo4 += effectController.velang4 * 2*Math.PI * (ahora4-antes4)/1000;			// Incrementar el angulo en 360Âº / sg
