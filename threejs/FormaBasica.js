90334fcc81873fb15d838a9b436da3d2887e6dc3
ELMA <GOKTUG@ELMA-MacBook-Pro.local>
2020-10-24 00:38:47 +0200

    Initial commit

--- threejs/FormaBasica.js
+++ threejs/FormaBasica.js
@@ -114,19 +114,7 @@ function init() {
 		renderer.render(scene, camera);		
 	});
 
-	var lastTimeMsec= null
-	requestAnimationFrame(function animate(nowMsec){
-		// keep looping
-		requestAnimationFrame( animate );
-		// measure time
-		lastTimeMsec	= lastTimeMsec || nowMsec-1000/60
-		var deltaMsec	= Math.min(200, nowMsec - lastTimeMsec)
-		lastTimeMsec	= nowMsec
-		// call each update function
-		updateFcts.forEach(function(updateFn){
-			updateFn(deltaMsec/1000, nowMsec/1000)
-		});
-	});
+	
 }
 
 function rotate(event) {
